874615f671878f47386e05b4891930af28c3ce11
Muzammil Hussain <muzammil.org73@gmail.com>
2025-11-21 22:37:19 +0500

    Add files via upload

---
+++ oop7.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+class rectangle{
+    private:
+    int length, width;
+    public:
+    rectangle(){
+        length=0;
+        width=0;
+    }
+    rectangle(int l , int w){
+        cout <<"parameterized constructer"<<endl;
+        length=l;
+        width=w;
+    }
+    void area(){
+        cout <<"area="<<length*width<<endl;
+    }
+
+};
+int main(){
+    rectangle r1;
+    rectangle r2(5,6);
+    r1.area();
+    r2.area();
+    return 0;
+}
+// #include <iostream>
+// using namespace std;
+// class rectangle{
+//     private:
+//     int length , width;
+//     public:
+//     rectangle(){
+//         cout <<"default constructer"<<endl;
+//         length=0;
+//         width=0;
+//     }
+//     rectangle(int l, int w){
+//         cout <<"parameterized constructer"<<endl;
+//         length=l;
+//         width=w;
+//     }
+//     void area(){
+//         cout <<"area"<<length*width<<endl;
+//     }
+// };
+// int main(){
+//     rectangle r1;
+//     rectangle r2(5,10);
+//     r1.area();
+//     r2.area();
+//     return 0;
+// }
