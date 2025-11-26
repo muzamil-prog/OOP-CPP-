a5f2844c30bfb95548d0a4450da4adfbded6a8f1
Muzammil Hussain <muzammil.org73@gmail.com>
2025-11-27 00:00:12 +0500

    Add files via upload

---
+++ expalin.cpp
@@ -0,0 +1,22 @@
+// // ChatGPT said:
+
+// In simple words, a static data member in OOP is a variable that belongs to the class itself, not to any one object.
+
+// So:
+
+// All objects of that class share the same variable.
+
+// It keeps one common value for all objects.
+
+// You can use it without creating an object (using the class name).
+
+// 🧩 Example idea:
+// If you have a class Student, and you want to keep track of how many students were created — that number should be the same for all students.
+// // So, you make it a static data member
+
+
+
+// static variable ik esa varible ha jo class sy  jur jata ha
+
+// static variable ik esa variable ha jasy sb objects share krty hian 
+
