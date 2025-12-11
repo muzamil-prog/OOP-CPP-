775935513b4c7fe6407b0e23fea7fcf613e4ddf5
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-11 22:41:16 +0500

    Add files via upload

---
+++ programe4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void sound() { cout << "Some sound\n"; }
+};
+
+class Dog : public Animal {
+public:
+    void sound() override { cout << "Dog barks\n"; }
+};
+
+int main() {
+    Animal* a = new Dog;
+    a->sound();
+    delete a;
+}
