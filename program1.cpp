6e017ab164b6ebe35b34f7ffd33ead4e86638859
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-13 19:01:00 +0500

    Add files via upload

--- program1.cpp
+++ program1.cpp
@@ -1,23 +1,28 @@
-#include<iostream>
+//return object from member function
+
+#include <iostream>
 using namespace std;
-class parent{
+class number{
+    int num;
     public:
-    void showParent(){
-        cout <<"this is a parent class"<<endl;
+    void getValue(int n){
+        num=n;
+    }
+    number add(number obj){
+        number temp;
+        temp.num=num+obj.num;
+        return temp;
+    }
+    void display(){
+        cout <<"number:"<<num<<endl;
     }
 };
-class child:public parent{
-    public:
-    void showChild(){
-        cout <<"this is a child class"<<endl;
-    }
-};
-
-int main (){
-    child ob;
-    ob.showParent();
-    ob.showChild();
-    
-
+int main(){
+    number m1,m2,m3;
+    m1.getValue(30);
+    m2.getValue(40);
+    m3=m1.add(m2);
+    cout <<"result after addtion:"<<endl;
+    m3.display();
     return 0;
 }
