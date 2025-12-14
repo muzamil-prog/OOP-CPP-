b770baa759916a2a42fe46f55e20469e75853e6a
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-14 22:42:09 +0500

    Add files via upload

--- programe4.cpp
+++ programe4.cpp
@@ -1,18 +1,29 @@
 #include <iostream>
 using namespace std;
-
-class Animal {
-public:
-    virtual void sound() { cout << "Some sound\n"; }
+class multiply{
+    int a;
+    int b;
+    
+    public:
+    void setValue(int a, int b){
+        this->a=a;
+        this->b=b;
+    }
+    multiply product(multiply obj){
+        multiply temp;
+        temp.a=a*obj.a;
+        temp.b=b*obj.b;
+        return temp;
+    }
+    void shoe(){
+        cout <<"the product fo a= "<<a<<"the product of b= "<<b<<endl;
+    }
 };
-
-class Dog : public Animal {
-public:
-    void sound() override { cout << "Dog barks\n"; }
-};
-
-int main() {
-    Animal* a = new Dog;
-    a->sound();
-    delete a;
+int main(){
+    multiply m1, m2 ,m3;
+    m1.setValue(6,8);
+    m2.setValue(8,7);
+    m3=m1.product(m2);
+    m3.shoe();
+    return 0;
 }
