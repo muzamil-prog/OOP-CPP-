ab56b1586653078ee330189bb7ad2d028ec17c11
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-22 21:13:34 +0500

    Add files via upload

--- programe9.cpp
+++ programe9.cpp
@@ -1,36 +1,22 @@
 #include <iostream>
 using namespace std;
-class salary{
-    private:
-    int basicSalary;
-    int bouns;
-    int total;
-    public:
-    salary():basicSalary(0),bouns(0),total(0){}
-    salary(int basicSalary, int bouns){
-        this->basicSalary=basicSalary;
-        this->bouns=bouns;
-        total= basicSalary+bouns;
-    }
-    salary Calculate(salary& obj){
-        salary result;
-        result.total= basicSalary+bouns;
-        // result.bouns=bouns+obj.bouns;
-        // result.basicSalary= basicSalary+obj.basicSalary;
-        // result.total=result.basicSalary+bouns;
-        return result;
-
-    }
-    void show(){
-        cout <<"total salary is:"<<total<<endl;
-    }
-
-     
+class mobile{
+private:
+string brand;
+int storage;
+public:
+void setValue(string brand, int storage){
+    this->brand=brand;
+    this->storage=storage;
+}
+void show(mobile &obj){
+    cout <<"the barand of moblie is:"<<obj.brand<<endl;
+    cout <<"the storage of the moblie:"<<obj.storage<<endl;
+}
 };
 int main(){
-    // salary s1(20000, 5000),s2(10000,2000),s3; for two salary 
-    salary s1(20000, 5000), s2;// for single salary
-    s2=s1.Calculate(s1);
-    s2.show();
+    mobile b1;
+    b1.setValue("vivo", 256);
+    b1.show(b1);
     return 0;
 }
