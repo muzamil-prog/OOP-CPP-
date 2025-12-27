5be0dfc600904db52a6bbf189836f8deac6c6b9d
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-27 21:59:33 +0500

    Add files via upload

---
+++ program9 (1).cpp
@@ -0,0 +1,6 @@
+// memory allocation
+// those things are coomen for all objects in class then make one function 
+// jo cheze commen hain sary objects ky laya class ma usy sirf ik bar bna do 
+// other wise alg alg bna do
+// jb bi hmm ik class bnaty hain wo sary object ky leya common hoti ha
+
