d4a0d0b296fc3fed7780ec14a38b4aab0f6785e8
Muzammil Hussain <muzammil.org73@gmail.com>
2025-12-28 22:05:25 +0500

    Add files via upload

--- program10.cpp
+++ program10.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 using namespace std;
-class employee{
+class shop{
+    private:
+    int itemId[100];
+    int itemPrice[100];
+    int counter=0;
     public:
-    string name;
-    int id;
-    public:
-    void setData(string n, int i){
-        name=n;
-        id=i;
-        
+    void counterId(){
+        counter=0;
     }
+    void setPrice();
+    void displayPrice();
 };
-class salary:public employee{
-    float basic, hra, da;
-    public:
-    void setSalary(float b, float h, float d){
-        basic=b; 
-        hra=h;
-        da=d;
-
+void shop:: setPrice(){
+    cout <<"enter the your item id"<<counter+1<<endl;
+    cin >>itemId[counter];
+    cout <<"enter the your item price"<<counter+1<<endl;
+    cin >>itemPrice[counter];
+    counter++;
+}
+void shop:: displayPrice(){
+    for(int i=0; i<counter; i++){
+        cout <<"yout price with item id"<<itemId[i]<<"is:"<<itemPrice[i]<<endl;
     }
-    void show(){
-        cout <<"employee name="<<name<<"ID="<<id<<endl;
-    };
-
+}
+int main(){
+    shop dukan;
+    dukan.counterId();
+    dukan.setPrice();
+    dukan.setPrice();
+    dukan.setPrice();
+    dukan.setPrice();
+   dukan.displayPrice();
+   return 0;
 }
