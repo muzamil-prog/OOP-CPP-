fefd1ab050a2db693f57b6b821842c8b468ba0c3
Muzammil Hussain <muzammil.org73@gmail.com>
2026-01-05 19:26:42 +0500

    Add files via upload

--- p10.cpp
+++ p10.cpp
@@ -1,23 +1,7 @@
-// inhertance help extend the functionality
-
-#include <iostream> 
+#include<iostream>
 using namespace std;
-class vehicle{
-    public:
-    void start(){
-        cout <<"vehucke stats movung "<<endl;
 
-    }
-};
-class Car:public vehicle{
-    public:
-    void accelerate(){
-        cout <<"car accelerates on road"<<endl;
-    }
-};
-int main(){
-    Car c;
-    c.start();
-    c.accelerate();
+int main (){
+    
     return 0;
 }
