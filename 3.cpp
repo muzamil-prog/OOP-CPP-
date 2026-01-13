cd680d53115f85ab3c415c1b5c41e637dcbb1f4d
Muzammil Hussain <muzammil.org73@gmail.com>
2026-01-13 22:16:42 +0500

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,20 @@
+// destruuucter : 
+
+// destructer is attomatically called when an object out of scope 
+// destructer is oppsite to constucter it meams that constructer are allocate the memory to object but destructer are deallocate the memory yani jo memory object ko di gi ha destructer ka kam ha usy saaf krna 
+
+
+// syntax
+//~class name(){
+   // body
+
+   //}
+//porperty 
+// 1 only one destructer is declare in classs
+// 2 no parameter is used in destructer
+//3 no return type 
+// 4 same as the name of class
+// 5 it reprsent with telda sign
+
+
+
